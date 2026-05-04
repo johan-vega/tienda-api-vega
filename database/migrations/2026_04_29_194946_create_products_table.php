76468cf349590a07641d02af940642a0f087f030
Andre Vega <johan.vi.em.99@gmail.com>
2026-05-04 14:19:50 -0500

    se sube el proyecto realizado con laravel

---
+++ database/migrations/2026_04_29_194946_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('sku')->unique();
+            $table->unsignedInteger('stock')->default(0);
+            $table->decimal('price', 10, 2);
+            $table->boolean('is_active')->default(true);
+            $table->foreignId('categories_id')->constrained('categories'); // esto hace la relacion
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
